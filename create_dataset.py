d2ad026c84d86a7d1d7ec98b178cae05540e022d
Dann38 <38dunn@gmail.com>
2024-10-08 16:02:35 +0800

    add creater dataset

---
+++ create_dataset.py
@@ -0,0 +1,34 @@
+from pager import PageModel, PageModelUnit
+from pager.page_model.sub_models import ImageModel, WordsAndStylesModel, SpGraph4NModel
+from pager.page_model.sub_models import ImageToWordsAndStyles,  WordsAndStylesToSpGraph4N
+from publaynet_reader import PubLayNetDataset 
+import argparse
+
+page = PageModel([
+    PageModelUnit(id="image", sub_model=ImageModel(), converters={}, extractors=[]),
+    PageModelUnit(id="word_and_style", sub_model=WordsAndStylesModel(), converters={"image": ImageToWordsAndStyles()}, extractors=[]),
+])
+
+def get_words_and_styles(img_path):
+    page.read_from_file(img_path)
+    page.extract()
+    return page.to_dict()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='create dataset for PageR from PubLayNet')
+    parser.add_argument('--path_publaynet', type=str, nargs='?', required=True,
+                        help='path publaynet')
+    parser.add_argument('--path_words_and_styles', type=str, nargs='?', required=True,
+                        help='path blocks, words and styles dataset')
+    parser.add_argument('--start', type=int, nargs='?', required=True,
+                        help='category exist') 
+    parser.add_argument('--finish', type=int, nargs='?', required=True,
+                        help='category will exist')
+                        
+    args = parser.parse_args()
+    pln_ds = PubLayNetDataset(args.path_publaynet, args.path_words_and_styles)
+    pln_ds.create_tmp_annotation_jsons(path_tmp_dataset=args.path_words_and_styles, 
+                                       fun_additional_info=get_words_and_styles, 
+                                       start_min_category= args.start, finish_min_category=args.finish)
+    
+    
